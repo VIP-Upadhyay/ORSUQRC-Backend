824406253e8974b2a33ae8ab7be8c2d1e3b0648f
VIP-Upadhyay <74530514+VIP-Upadhyay@users.noreply.github.com>
2021-06-27 12:33:14 +0530

    Delete ORSUQRC directory

--- ORSUQRC/src/main/java/com/vip/orsuqrc/controller/rest/LoginController.java
+++
@@ -1,9 +0,0 @@
-package com.vip.orsuqrc.controller.rest;
-
-public class LoginController {
-
-	public LoginController() {
-		// TODO Auto-generated constructor stub
-	}
-
-}
--- ORSUQRC/src/main/java/com/vip/orsuqrc/service/security/SecurityLogin.java
+++
@@ -1,6 +0,0 @@
-package com.vip.orsuqrc.service.security;
-
-public interface SecurityLogin {
-	String findLoggedInUsername();
-	void autoLogin(String username,String Password);
-}
